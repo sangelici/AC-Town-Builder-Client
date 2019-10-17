1db0c5c1110513ff5a75bbfa737db9b892355a11
sangelici <sangelici94@gmail.com>
2019-10-17 14:53:09 -0400

    Created auth curl-scripts connected to heroku

---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "https://salty-scrubland-96340.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/auth/sign-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "https://salty-scrubland-96340.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
